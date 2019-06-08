0a60466e8ca318dae5671c863d05cf6f2388886b
Nikita Kovalev <just13kiy@gmail.com>
2019-06-08 03:40:13 +0300

    added 'logging'

--- MainBot.py
+++ MainBot.py
@@ -12,12 +12,14 @@ import telebot
  
 
 
-def start(bot, update): 
-   update.message.reply_text('Hello {}:'.format(update.message.from_user.first_name))
+def start(bot, update):
+    print('Bot has started polling')
+    update.message.reply_text('Hello {}:'.format(update.message.from_user.first_name))
 
     
 
-def hello(bot, update): 
+def hello(bot, update):
+    print('Starting conversation')
     #update.message.reply_text('Your photo {}:'.format(update.message.from_user.first_name))
     chat_id = bot.get_updates()[-1].message.chat_id
     #bot.send_photo(chat_id, open("C:/Program1/Projects/Python/Car-Segmentation/test_1_yolo_out_py.jpg", 'rb')) 
@@ -27,7 +29,8 @@ def hello(bot, update):
     #bot.send_photo(chat_id, "FILEID")
     update.message.reply_text('Your photo {}:'.format(update.message.from_user.first_name))
 
-def photo(bot, update):  
+def photo(bot, update):
+    print('Took photo')
     test = 'test_'
     numb = 1
     save_path = 'C:/Program1/Projects/Python/Car-Segmentation' 
@@ -35,8 +38,12 @@ def photo(bot, update):
     newFile = bot.getFile(file_id) 
     newFile.download(os.path.join(save_path, test + str(numb) +'.jpg')) 
     bot.sendMessage(chat_id=update.message.chat_id, text="download succesful") 
-    filename = ( test + str(numb+1) +'.jpg') 
+    filename = ( test + str(numb+1) +'.jpg')
+
+    print('starting serving photo')
     import car_detection_yolo
+    print('photo is ready')
+
     with open(filename,"rb") as f: 
      Jpegcontents = (f.read()) 
      if Jpegcontents.startswith(b"\xff\xd8") and Jpegcontents.endswith(b"\xff\xd9"): 
@@ -44,12 +51,15 @@ def photo(bot, update):
      if not Jpegcontents.startswith(b"\xff\xd8") and Jpegcontents.endswith(b"\xff\xd9"): 
       os.system("rm ",filename)
 
+print('Initializing')
 
 photo_handler = MessageHandler(Filters.photo, photo) 
 updater.dispatcher.add_handler(photo_handler) 
 updater.dispatcher.add_handler(CommandHandler('start', start)) 
 updater.dispatcher.add_handler(CommandHandler('get', hello)) 
 
-updater.start_polling() 
-updater.idle() 
-updater.stop() 
+print('Car detector is ready')
+
+updater.start_polling()
+updater.idle()
+updater.stop()
